d4bf304a8a9c2cec6cdca5184bd74e2f8010a3f7
Andrey Velikorechin <asvelikorechin@gmail.com>
2023-05-16 23:11:45 +0300

    добавил свои моедли и зарегистрировал их в админке

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -46,10 +46,60 @@ class User(AbstractUser):
         return self.username
 
 
+class Category(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название категории',
+        unique=True
+    )
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название жанра',
+        unique=True
+    )
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
 class Title(models.Model):
-    ''' Модель заглушка требует доработки '''
-    # TODO: "Нужно импортировать сюда модель Андрея"
-    pass
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название произведения')
+    year = models.PositiveSmallIntegerField(verbose_name='Год выпуска')
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Описание')
+    genres = models.ManyToManyField(Genre)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        verbose_name='Категория'
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
 
 
 class Review(models.Model):
