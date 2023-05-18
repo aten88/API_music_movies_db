cda0d0971bc7454317b04ae1fad308c2000d3fcd
aten88 <virtuoso_ten@mail.ru>
2023-05-18 17:38:24 +0300

    Исправил замечания Саши

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,10 +3,15 @@ from rest_framework import routers
 from api.views import ReviewViewSet, CommentViewSet
 
 router = routers.DefaultRouter()
-router.register(r'reviews', ReviewViewSet)
-router.register(r'comments', CommentViewSet)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet, basename='review'
+)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet, basename='comment'
+)
 
 urlpatterns = [
-    path('api/v1/', include(routers.urls)),
-
+    path('v1/', include(router.urls)),
 ]
