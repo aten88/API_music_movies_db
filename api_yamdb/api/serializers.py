2617f61e59f1c4a7cca563413527c8df932cbb24
Alexandr Gnatyuk <steellgear@gmail.com>
2023-05-19 09:59:51 +0300

    поправил по pep8

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-from reviews.models import Review, Comment
 from django.contrib.auth import get_user_model
+
+from reviews.models import Review, Comment
 
 User = get_user_model()
 
