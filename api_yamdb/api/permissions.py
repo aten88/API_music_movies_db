feba8c1e0f23a5b380723a654ca9119c79e8641f
Alexandr Gnatyuk <steellgear@gmail.com>
2023-05-20 17:04:00 +0300

    лишнее долой, небольшие исправления

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,8 +1,7 @@
 from rest_framework import permissions
 
 
-
-class IsAdminOrReadOnly(BasePermission):
+class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
